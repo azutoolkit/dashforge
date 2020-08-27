fde223542249b10493281b335aac60e458491d6b
Elias J. Perez <eliasjpr@gmail.com>
2020-08-26 22:37:52 -0400

    Adds queue details and jobs per second

--- src/views/queues/index.cr
+++ src/views/queues/index.cr
@@ -1,12 +1,15 @@
 module Queues
   struct Index
     include Azu::Html
+    getter joobq = JoobQ.statistics
 
     def initialize(@name : String)
     end
 
     def html
       render "queues/index.jinja", {
+        "queue" => joobq.queue(@name),
+        "jobs_spark" => Queues::JobsSpark.new(@name).to_s(IO::Memory.new).to_s,
         "errors_spark" => Queues::ErrorsSpark.new(@name).to_s(IO::Memory.new).to_s,
         "latency_spark" => Queues::LatencySpark.new(@name).to_s(IO::Memory.new).to_s
       }
